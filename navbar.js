1ad2b7c96ef0af2fcb637c55309d8092e406fd89
Arcadi Garcia <arcadigarciarius21@gmail.com>
2017-12-25 23:04:23 +0100

    arreglat navbar i posada up to date

--- navbar.js
+++ navbar.js
@@ -1,16 +1,16 @@
 function responsiveNavbar() {
-  //afegir logo
-  $("#logo-ph").attr("src","imgs/logotippetop.png");
-
   //posicionar logo
   var h = $("#navul .gente a")[0].getBoundingClientRect().height;
   var pa = $("#navul li a").css("padding-top");
+  //afegir logo
+  $("#logo-ph").attr("src","imgs/logotippetop.png");
   pa = Number(pa.slice(0,pa.length-2));
-  $("#logo-ph").offset().top = 0;
 
-  $("#logo-ph").css({"height": h-pa + "px"});
-  $("#logo-ph").parent().css({"padding-top": pa/2.0+2 + "px",
-    "padding-bottom": pa/2.0-2 + "px"});
+  var x = 4;
+  $("#logo-ph").css({"height": h-pa + "px",
+    "margin-top": -x + "px"});
+  $("#logo-ph").parent().css({"padding-top": pa/2.0+x + "px",
+    "padding-bottom": pa/2.0-x + "px"});
 
   //posicionar la resta de la pàgina
   h = $("#navul")[0].getBoundingClientRect().height;
